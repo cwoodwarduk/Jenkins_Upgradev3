38f59259a5493743322ac5ba017c51442d120d14
CWoodward <calum.woodward@bestenergysaving.com>
2023-03-27 07:03:08 +0100

    changing tests

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -11,14 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
-
   
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 
-  
   public static void main(String args[]){
     
 	  System.out.println("Hello from Main Method!!!");
