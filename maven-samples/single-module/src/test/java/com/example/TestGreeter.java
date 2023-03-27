d1c61c54ad67e7244b2b50c0a1cc587fa852d28f
CWoodward <calum.woodward@bestenergysaving.com>
2023-03-27 05:54:51 +0100

    removing test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,11 +40,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
-     @Test
-    public void greetShouldIncludeGreetingMessage() {
-      String someone = "Hello World";
-
-      assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-    }
 }
